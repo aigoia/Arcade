b37611ce48c3b82e8a9c149036418c3ddda74a79
aigoia <oidoicompany@gmail.com>
2025-01-17 21:25:54 +0900

    cleaning

---
+++ ArcadeUI/OperationButton.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace ArcadeUI
+{
+    public class OperationButton : MonoBehaviour
+    {
+        public Transform Background => transform.Find("Background");
+        public Animator Animator => GetComponent<Animator>();
+        public Button Button => GetComponent<Button>();
+    }
+}
--- ArcadeUI/OperationButtonManager.cs
+++ ArcadeUI/OperationButtonManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using ArcadeUI;
 using UnityEngine;
 using UnityEngine.UI;
 using Image = UnityEngine.UI.Image;
